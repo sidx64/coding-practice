1808a41177d8a28e585a36460132b39179063ec5
Siddharth Shanbhogue <siddharthx64@gmail.com>
2021-08-21 21:05:52 +0530

    added new codewar attempt

---
+++ js/codewars-categorize-new-member.js
@@ -0,0 +1,46 @@
+/*
+The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an 
+application form that will tell prospective members which category they will be placed.
+
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps 
+range from -2 to +26; the better the player the lower the handicap.
+
+INPUT
+Input will consist of a list of lists containing two items each. Each list contains information for a single potential member. 
+Information consists of an integer for the person's age and an integer for the person's handicap.
+
+Example Input
+[[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]]
+
+OUTPUT
+Output will consist of a list of string values (in Haskell: Open or Senior) stating whether the respective member is to be 
+placed in the senior or open category.
+
+Example Output
+["Open", "Open", "Senior", "Open", "Open", "Senior"]
+*/
+
+function openOrSenior(data) {
+  result = [];
+  if (!data.length) {
+    console.log(data);
+    return [];
+  } else {
+    data.forEach((element) => {
+      if (element[0] >= 55 && element[1] > 7) {
+        result.push('Senior');
+      } else {
+        result.push('Open');
+      }
+    });
+  }
+  console.log(result);
+  return result;
+}
+
+openOrSenior([
+  [45, 12],
+  [55, 21],
+  [19, -2],
+  [104, 20],
+]);
