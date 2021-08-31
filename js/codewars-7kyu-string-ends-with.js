f60ee1cc1773010b82e421094a8bb016c870bfe9
Siddharth Shanbhogue <siddharthx64@gmail.com>
2021-09-01 00:29:17 +0530

    added 7kyu problem

---
+++ js/codewars-7kyu-string-ends-with.js
@@ -0,0 +1,19 @@
+/* 
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+
+*/
+
+function solution(str, ending) {
+    // TODO: complete
+    // return str.slice(str.length - ending.length, str.length) === ending;
+
+    return str.endsWith(ending);
+}
+
+solution('abc', 'bc'); // returns true
+solution('abc', 'd'); // returns false
