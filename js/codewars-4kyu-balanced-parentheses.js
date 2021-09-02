5beee66b1ff7f1658626aa981c37c6e41ec985b8
Siddharth Shanbhogue <siddharthx64@gmail.com>
2021-09-03 00:13:33 +0530

    added first 4KYU solution

--- js/codewars-4kyu-balanced-parentheses.js
+++ js/codewars-4kyu-balanced-parentheses.js
@@ -14,7 +14,40 @@ the i’th character can be ‘}’ if and only if the count of ‘{‘ is great
 */
 
 function balancedParens(n) {
-    if (n === 0) return [];
+    if (n === 0) return [''];
+    if (n === 1) return ['()'];
 
-    let stack = new Array(n).fill(0);
+    let stack = [];
+    let result = [];
+
+    // total open must equal total close
+    // add closedParentheses only if [closedParentheses < openParentheses]
+
+    // n = 3
+    // openP = 1
+    // closeP = 0
+    // stack => ['(']
+
+    function backTrack(openP, closeP) {
+        if (openP === n && closeP === n) {
+            result.push(stack.join(''));
+        }
+
+        if (openP < n) {
+            stack.push('(');
+            backTrack(openP + 1, closeP);
+            stack.pop();
+        }
+        if (closeP < openP) {
+            stack.push(')');
+            backTrack(openP, closeP + 1);
+            stack.pop();
+        }
+    }
+
+    backTrack(0, 0);
+    console.log(result);
+    return result;
 }
+
+balancedParens(3);
